67491f8bbdb31e064616352434979548b001a96b
donatadefi <donatadefi@protonmail.com>
2021-08-28 11:08:26 +0800

    update info

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -293,32 +293,7 @@ function Home() {
                   <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
                   <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
                 </svg>
-                <p>485,000 presale (48.5%)</p>
-              </div>
-              <div className="tokenomics-list">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-circle-dashed"
-                  width="60"
-                  height="60"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="#ff4495"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
-                  <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
-                  <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
-                  <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
-                  <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
-                  <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
-                  <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
-                  <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
-                </svg>
-                <p>485,000 uniswap liquidity (48.5%)</p>
+                <p>No presale</p>
               </div>
             </div>
           </div>
